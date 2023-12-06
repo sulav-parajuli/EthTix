cd4e3d6602bc999794c1038c229f0d9f1a338d66
Sulav Parajuli <sulavparajuli82@gmail.com>
2023-12-06 12:10:29 +0545

    contractAddresses.json, favicon

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,14 +5,25 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   const Tickets = await hre.ethers.getContractFactory("Tickets");
   const tickets = await Tickets.deploy();
 
-  await tickets.waitForDeployment();
+  await tickets.deployed();
 
-  console.log("Tickets deployed to:", tickets.target);
+  console.log("Tickets deployed to:", tickets.address);
+
+  // Write contract addresses to a JSON file
+  const addresses = {
+    // user: user.target,
+    tickets: tickets.address,
+  };
+
+  fs.writeFileSync("contractAddresses.json", JSON.stringify(addresses));
+
+  console.log("Contract addresses written to contractAddresses.json");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
