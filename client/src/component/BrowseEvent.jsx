719994d7c3f0d4fb5c04aa03b12719f3c7222dea
Sulav Parajuli <sulavparajuli82@gmail.com>
2023-12-19 13:59:51 +0545

    git merge

--- client/src/component/BrowseEvent.jsx
+++ client/src/component/BrowseEvent.jsx
@@ -116,85 +116,91 @@ const BrowseEvent = ({ state }) => {
           <p className="main-text">Events</p>
         </div>
         <div>
+          {/* <div className="text-block "> */}
+
           <div className="event-blocks">
             {events.length === 0 ? (
               <p>Events not available....</p>
             ) : (
-              events.map((event, index) => (
-                // <div key={index}>
-                //   <h2>{event.eventName.toString()}</h2>
-                //   <p>
-                //     Price: {ethers.utils.formatEther(event.price).toString()}{" "}
-                //     ETH
-                //   </p>
-                //   <p>Total Tickets: {event.totalTickets.toNumber()}</p>
+              <div className="row">
+                {events.map((event, index) => (
+                  // <div key={index}>
+                  //   <h2>{event.eventName.toString()}</h2>
+                  //   <p>
+                  //     Price: {ethers.utils.formatEther(event.price).toString()}{" "}
+                  //     ETH
+                  //   </p>
+                  //   <p>Total Tickets: {event.totalTickets.toNumber()}</p>
 
-                //   <p>Location: {event.location.toString()}</p>
-                //   {/* <p>Creator: {event.creator}</p> */}
-                //   <p>
-                //     Timestamp:{" "}
-                //     {new Date(event.timestamp.toNumber()).toLocaleString()}
-                //   </p>
-                //   <button>Buy Ticket</button>
-                // </div>
-                <div key={index}>
-                  <div className="container">
-                    <div className="row justify-space-between py-2">
-                      <div className="col-6 mx-auto">
-                        <div className="card shadow-lg mt-4">
-                          <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                            <a className="d-block blur-shadow-image">
-                              <img
-                                src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
-                                alt="img-blur-shadow"
-                                className="img-fluid shadow border-radius-lg"
-                              />
-                            </a>
-                          </div>
-                          <div className="card-body">
-                            <h4>{event.eventName.toString()}</h4>
-                            <p>
-                              Price:{" "}
-                              {ethers.utils.formatEther(event.price).toString()}{" "}
-                              ETH
-                            </p>
-                            <p>
-                              Total Tickets: {event.totalTickets.toNumber()}
-                            </p>
+                  //   <p>Location: {event.location.toString()}</p>
+                  //   {/* <p>Creator: {event.creator}</p> */}
+                  //   <p>
+                  //     Timestamp:{" "}
+                  //     {new Date(event.timestamp.toNumber()).toLocaleString()}
+                  //   </p>
+                  //   <button>Buy Ticket</button>
+                  // </div>
+                  <div key={index} className="col-4 mb-4">
+                    <div className="container">
+                      <div className="row justify-space-between py-2">
+                        <div className="col-6 mx-auto">
+                          <div className="card shadow-lg mt-4">
+                            <div className="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                              <a className="d-block blur-shadow-image">
+                                <img
+                                  src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
+                                  alt="img-blur-shadow"
+                                  className="img-fluid shadow border-radius-lg"
+                                />
+                              </a>
+                            </div>
+                            <div className="card-body">
+                              <h4>{event.eventName.toString()}</h4>
+                              <p>
+                                Price:{" "}
+                                {ethers.utils
+                                  .formatEther(event.price)
+                                  .toString()}{" "}
+                                ETH
+                              </p>
+                              <p>
+                                Total Tickets: {event.totalTickets.toNumber()}
+                              </p>
 
-                            <p>Location: {event.location.toString()}</p>
-                            {/* <p>Creator: {event.creator}</p> */}
-                            <p>
-                              Timestamp:{" "}
-                              {new Date(
-                                event.timestamp.toNumber()
-                              ).toLocaleString()}
-                            </p>
-                            <div className="buttons">
-                              <button
-                                className="icon-move-right main-button color-white"
-                                onClick={handleOpenPopup}
-                              >
-                                Buy Ticket
-                                {/* <i
+                              <p>Location: {event.location.toString()}</p>
+                              {/* <p>Creator: {event.creator}</p> */}
+                              <p>
+                                Date and Time :{" "}
+                                {new Date(
+                                  event.timestamp.toNumber()
+                                ).toLocaleString()}
+                              </p>
+                              <div className="buttons">
+                                <button
+                                  className="icon-move-right main-button color-white"
+                                  onClick={handleOpenPopup}
+                                >
+                                  Buy Ticket
+                                  {/* <i
                               className="fas fa-arrow-right text-xs ms-1"
                               aria-hidden="true"
                             ></i> */}
-                              </button>
+                                </button>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
+                    <Popup
+                      isOpen={isPopupOpen}
+                      onClose={handleClosePopup}
+                      ke={index}
+                      event={event}
+                    />
                   </div>
-                  <Popup
-                    isOpen={isPopupOpen}
-                    onClose={handleClosePopup}
-                    ke={index}
-                    event={event}
-                  />
-                </div>
-              ))
+                ))}
+              </div>
             )}
           </div>
         </div>
